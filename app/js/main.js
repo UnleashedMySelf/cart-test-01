ddace56545a71460a3b7343042e2f43ac2ea032f
UnleashedMySelf <unleashedmyself@gmail.com>
2021-05-13 10:38:48 +0300

    added some logic

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,52 @@ const hideCart = document.querySelector('.btn__cart-close');
 const cart = document.querySelector('.cart');
 const hideCartOverlay = document.querySelector('.cart-overlay');
 
+const shippingCost = 0;
+
+const allItems = document.querySelector('.cart__items')
+const productItemList = [...document.querySelectorAll('.cart__item')];
+const countChange = document.querySelectorAll('.cart__item-input-qty');
+const itemPrice = document.querySelectorAll('.cart__item-price');
+const inputValue = (input) => Number(input.value) * Number(input.dataset.price);
+let countOfItems = document.querySelector('.cart-indicator');
+
+const main = () => {
+    let totalPrice = 0;
+    productItemList.forEach((productItem) => {
+        totalPrice += inputValue(productItem.querySelector('.cart__item-input-qty'));
+        console.log(inputValue(productItem.querySelector('.cart__item-input-qty')));
+        document.querySelector('.cart__totalcost-amout').textContent = '$ ' + totalPrice;
+    })
+    if (productItemList.length <= 0) {
+        document.querySelector('.cart__totalcost-amout').textContent = '$0';
+        allItems.innerHTML += ``;
+    }
+    totalPrice += shippingCost;
+    countOfItems.textContent = '(' + productItemList.length + ')';
+    console.log(countOfItems);
+    document.querySelector('.cart__totalcost-amout').textContent = '$' + totalPrice;
+} 
+main();
+
+const subTotal = () => {
+    countChange.forEach(inputChangeVal => {
+        inputChangeVal.addEventListener('change', (e) => {
+            const targetItem = e.target;
+            if (targetItem.value <= 0) {
+                targetItem.value = 1
+            }
+            //itemPrice.forEach(itemPr => {
+            //    target.itemPr = '$ ' + Number(targetItem.value) * Number(targetItem.dataset.price);
+            //})
+            console.log(targetItem);
+            //targetItem.itemPrice = '$ ' + Number(targetItem.value) * Number(targetItem.dataset.price);
+            main();
+            e.preventDefault();
+        })
+    })
+}
+subTotal();
+
 
 hideCartOverlay.addEventListener('click', () => {
     cart.classList.remove('cart-visible');
