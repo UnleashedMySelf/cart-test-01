e2958be0547217d473bd4144b1e1a0c0821df90e
UnleashedMySelf <unleashedmyself@gmail.com>
2021-05-18 15:36:28 +0300

    version with X (logic)

--- app/js/main.js
+++ app/js/main.js
@@ -5,13 +5,14 @@ const hideCartOverlay = document.querySelector('.cart-overlay');
 
 const shippingCost = 0;
 
-const allItems = document.querySelector('.cart__items')
+const allItems = document.querySelector('.cart__items');
 const productItemList = [...document.querySelectorAll('.cart__item')];
 const countChange = document.querySelectorAll('.cart__item-input-qty');
 const itemPrice = document.querySelectorAll('.cart__item-price');
 const inputValue = (input) => Number(input.value) * Number(input.dataset.price);
 let countOfItems = document.querySelector('.cart-indicator');
 const btnQty = document.getElementById('cartItems');
+const btnDeleteItem = document.querySelectorAll('.btn__cart-item-close');
 console.log(btnQty);
 
 const main = () => {
@@ -27,6 +28,25 @@ const main = () => {
     }
     totalPrice += shippingCost;
     countOfItems.textContent = '(' + productItemList.length + ')';
+    countOfItems.dataset.indicator = productItemList.length;
+
+    if (countOfItems.dataset.indicator == 0) {
+        cart.classList.add('cart-empty');
+        countOfItems.textContent = '';
+        document.querySelector('.cart__shipping-unlock').classList.add('hidden');
+        document.querySelector('.cart__checkout-total').classList.add('hidden');
+        document.querySelector('.cart__items-empty-img').classList.remove('hidden');
+        document.querySelector('.cart__text-empty').classList.remove('hidden');
+        document.querySelector('.btn__cart-empty').classList.remove('hidden');
+        allItems.classList.add('cart__items-empty')
+        
+    }
+    else {
+        document.querySelector('.cart__items-empty-img').classList.add('hidden');
+        document.querySelector('.cart__text-empty').classList.add('hidden');
+        document.querySelector('.btn__cart-empty').classList.add('hidden');
+    }
+
     console.log(countOfItems);
     document.querySelector('.cart__totalcost-amout').textContent = totalPrice;
 } 
@@ -39,11 +59,8 @@ const subTotal = () => {
             if (targetItem.value <= 0) {
                 targetItem.value = 1
             }
-            //itemPrice.forEach(itemPr => {
-            //    target.itemPr = '$ ' + Number(targetItem.value) * Number(targetItem.dataset.price);
-            //})
-            console.log(targetItem);
-            //targetItem.itemPrice = '$ ' + Number(targetItem.value) * Number(targetItem.dataset.price);
+            const setSubtotalPrice = targetItem.closest('div').getElementsByClassName('cart__item-price')[0];
+            setSubtotalPrice.textContent = '$' + Number(targetItem.value) * Number(targetItem.dataset.price);
             main();
             e.preventDefault();
         })
@@ -54,22 +71,24 @@ subTotal();
 
 btnQty.addEventListener('click', (e) => {
     if(e.target.classList.contains('cart__item-qty-minus')) {
-        //calculatedItems {
-         //   e.target.closet('.cart__items'),
-           // 'minus'
-        //}
-        console.log('-');
+        const inputCurrent = e.target.closest("div").getElementsByClassName('cart__item-input-qty')[0];
+        inputCurrent.value <= 1 ? inputCurrent.value = 1 : inputCurrent.value--;
+        const setSubtotalPrice = e.target.closest("div").getElementsByClassName('cart__item-price')[0];
+        setSubtotalPrice.textContent = '$' + Number(inputCurrent.value) * Number(inputCurrent.dataset.price);
+        main();
+        subTotal();
     }
     if(e.target.classList.contains('cart__item-qty-plus')) {
-        //calculatedItems {
-         //   e.target.closet('.cart__items'),
-           // 'minus'
-        //}
-        console.log('+');
+        const inputCurrent = e.target.closest("div").getElementsByClassName('cart__item-input-qty')[0];
+        inputCurrent.value++;
+        const setSubtotalPrice = e.target.closest("div").getElementsByClassName('cart__item-price')[0];
+        setSubtotalPrice.textContent = '$' + Number(inputCurrent.value) * Number(inputCurrent.dataset.price);
+        main();
+        subTotal();
     }
 })
 
-
+// Show-hide cart
 hideCartOverlay.addEventListener('click', () => {
     cart.classList.remove('cart-visible');
     hideCartOverlay.classList.remove('hide-cart-overlay');
@@ -85,14 +104,14 @@ toggleCart.addEventListener('click', () => {
 
 
 
-/* // Цикл обработки удаления товаров по кнопке "удалить"
+// Цикл обработки удаления товаров по кнопке "удалить"
 btnDeleteItem.forEach(btnDel => {
     btnDel.addEventListener('click', () => {
-        const indexItem = productItemList.indexOf(btnDel.parentElement)
+        const indexItem = productItemList.indexOf(btnDel.parentElement);
         console.log(productItemList[indexItem]);
-        productItemList[indexItem].remove()
-        productItemList.splice(indexItem, 1)
-        main()
-        subTotal()  
+        productItemList[indexItem].remove();
+        productItemList.splice(indexItem, 1);
+        main();
+        subTotal();
     })
-}) */
+}) 
